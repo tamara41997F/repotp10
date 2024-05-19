9390db7ca53ae014cfcf1a96bffc680671a291f3
tamara41997F <166946784+tamara41997F@users.noreply.github.com>
2024-05-19 15:53:41 -0300

    Add files via upload

---
+++ funcionalidades.js
@@ -0,0 +1,24 @@
+var nombre = "Tamara";//String
+var apellido ="Gonzalez";//String
+
+var resultado;
+
+resultado = nombre + " " + apellido;
+
+console.log(resultado); // Salida: Tamara Gonzalez
+
+/* Segundo Ejercicio*/
+
+var numero1 = 3;
+var numero2 = 5;
+
+var suma;
+
+suma = numero1 + numero2;
+
+console.log(suma); //Salida: 8
+
+var numero3 = suma + 2;
+
+console.log(numero3); //Salida:10
+
